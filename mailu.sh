0cf2391dc6c0e1d3863111c1a815c81e1d02fd9f
Buddhi Thilakshana <hellobachchi@gmail.com>
2023-04-06 14:34:07 +0530

    new

--- mailu.sh
+++ mailu.sh
@@ -1,9 +1,9 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install \
     ca-certificates \
     curl \
-    gnupg
+    gnupg -y
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
