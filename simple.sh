02d6aec4a0bc5263203680ec9eefc765abba8a16
Buddhi Thilakshana <hellobachchi@gmail.com>
2023-04-06 16:23:38 +0530

    apt-get install python -y

--- simple.sh
+++ simple.sh
@@ -5,14 +5,14 @@ apt-get install python -y
 # This script starts a Python web server on port 8080 that responds with "Hello, World!"
 
 # Check if Python is installed
-if ! command -v python &> /dev/null
+if ! command -v python3 &> /dev/null
 then
     echo "Python is not installed. Aborting."
     exit 1
 fi
 
 # Start the Python web server
-python -m SimpleHTTPServer 80 &
+python3 -m SimpleHTTPServer 80 &
 
 # Print a message indicating that the server is running
 echo "Server is running on port 80."
